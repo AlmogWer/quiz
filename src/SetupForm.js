698feccfb10f5c084d9e7f4dee4fa050d1ed1296
Almog Wertzberger <Almogish@gmail.com>
2021-09-19 12:41:22 +0300

    adding setup quiz

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -1,8 +1,69 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
-}
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
+  return (
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+          <h2>setup quiz</h2>
+          {/* {amount} */}
+          <div className="form-control">
+            <label htmlFor="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              value={quiz.amount}
+              onChange={handleChange}
+              className="form-input"
+              min={1}
+              max={50}
+            />
+          </div>
+          {/* {category} */}
+          <div className="form-control">
+            <label htmlFor="category">category</label>
+            <select
+              name="category"
+              id="category"
+              className="form-input"
+              value={quiz.category}
+              onChange={handleChange}
+            >
+              <option value="sports">Sports</option>
+              <option value="history">History</option>
+              <option value="politics">Politics</option>
+            </select>
+          </div>
+          {/* {difficulty} */}
+          <div className="form-control">
+            <label htmlFor="difficulty">Select difficulty</label>
+            <select
+              name="difficulty"
+              id="difficulty"
+              className="form-input"
+              value={quiz.difficulty}
+              onChange={handleChange}
+            >
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="Hard">Hard</option>
+            </select>
+          </div>
+          {error && (
+            <p className="error">
+              can't generate questions, please try different options
+            </p>
+          )}
+          <button type="submit" onClick={handleSubmit} className="submit-btn">
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
 
-export default SetupForm
+export default SetupForm;
--- src/context.js
+++ src/context.js
@@ -22,6 +22,11 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0);
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    catagory: "sports",
+    difficulty: "easy",
+  });
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const fetchQuestions = async (url) => {
@@ -73,9 +78,13 @@ const AppProvider = ({ children }) => {
     setIsModalOpen(false);
   };
 
-  useEffect(() => {
-    fetchQuestions(tempUrl);
-  }, []);
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <AppContext.Provider
@@ -90,6 +99,9 @@ const AppProvider = ({ children }) => {
         nextQuestion,
         checkAnswer,
         closeModal,
+        quiz,
+        handleChange,
+        handleChange,
       }}
     >
       {children}
