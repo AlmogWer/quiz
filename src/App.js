7f4e31b2b5ad1bb799d00255ec696a861fcfe403
Almog Wertzberger <Almogish@gmail.com>
2021-09-19 11:59:46 +0300

    displaying question

--- src/App.js
+++ src/App.js
@@ -12,7 +12,33 @@ function App() {
   if (loading) {
     return <Loading />;
   }
-  return <main>quiz app</main>;
+  const { question, incorrect_answers, correct_answer } = questions[0];
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers:{correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
